681c6096c37b529f9c59de5288ea6f4630e4fe43
Ganesh Chapagain <tekchapagain1@gmail.com>
2021-11-24 14:20:47 +0545

    views modified

--- weather/views.py
+++ weather/views.py
@@ -1,10 +1,18 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    urls = 'api.openweathermap.org/data/2.5/weather?q={}&appid=fd8693717fdb7011ddc4fcb19d74dcd4'
+    urls = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=fd8693717fdb7011ddc4fcb19d74dcd4'
     city = 'Kathmandu'
 
-    r = requests.get(urls.format(city))
-    print(r.text)
-    return render(request,'weather/weather.html')
+    r = requests.get(urls.format(city)).json()
+    city_weather={
+        'city':city,
+        'temperature':r['main']['temp'],
+        'description':r['weather'][0]['description'],
+        'icon':r['weather'][0]['icon'],
+    }
+    
+    context = {'city_weather':city_weather}
+    return render(request,'weather/weather.html',context)
